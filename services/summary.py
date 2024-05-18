23500d77fbcd42ccb28af88ad58b88c2395ff7f6
azabarnahaju <kanikalexandra@gmail.com>
2024-05-18 14:08:33 +0200

    Add docstrings to summary service

--- services/summary.py
+++ services/summary.py
@@ -2,6 +2,7 @@ from datetime import datetime, timezone
 
 
 def create_summary(data):
+    """Creates summary of an individual observation"""
     summary_info = data["resource"]
     summary = {
         "observationId": summary_info.get("id"),
@@ -17,22 +18,26 @@ def create_summary(data):
 
 
 def get_patient_id(summary_info):
+    """Return Patient ID"""
     if "subject" in summary_info:
         return summary_info["subject"]["reference"].replace("Patient/", "")
     return None
 
 
 def get_formatted_current_utc_datetime():
+    """Return current UTC time formatted according to ISO"""
     return str(datetime.now(timezone.utc).isoformat()).split(".", 1)[0]+"Z"
 
 
 def get_performer_id(summary_info):
+    """Return Performer ID"""
     if "performer" in summary_info:
         return summary_info["performer"][0]["reference"].replace("Practitioner/", "")
     return None
 
 
 def get_measurement_coding(summary_info):
+    """Return measurement codings with LOINC system for an individual observation"""
     if "code" in summary_info and "coding" in summary_info["code"]:
         for coding in summary_info["code"]["coding"]:
             if "system" in coding and "http://loinc.org" in coding["system"]:
@@ -40,6 +45,7 @@ def get_measurement_coding(summary_info):
 
 
 def get_measurement_information(summary_info, data_type):
+    """Return measurement information for an individual observation based on the data type given (value | unit)"""
     if "valueQuantity" in summary_info:
         if data_type in summary_info["valueQuantity"]:
             return [summary_info["valueQuantity"][data_type]]
